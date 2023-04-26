8b81758f751ef13c4409a15f64c6ec42587d69d9
prem-pxtel <prempatel149@gmail.com>
2023-04-25 22:23:27 -0400

    cleaned up classes

--- gains_tracker_pro/lib/classes.dart
+++ gains_tracker_pro/lib/classes.dart
@@ -3,15 +3,15 @@ class Workout {
   int month = 0;
   int day = 0;
   int year = 0;
-  int numEx = 0;
+  List<Exercise> exList = [];
 }
 
-class Exercise extends Workout {
+class Exercise {
   String exName = '';
   String notes = '';
-  int numSets = 0;
+  List<Set> setList = [];
 }
 
-class Set extends Exercise {
+class Set {
   int repCount = 0;
 }
