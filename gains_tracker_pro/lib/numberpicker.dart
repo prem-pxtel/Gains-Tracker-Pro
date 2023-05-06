0c5ee615915fe181d92587be190d6af295184795
prem-pxtel <prempatel149@gmail.com>
2023-05-06 02:58:43 -0400

    cleaned up numberpicker

--- gains_tracker_pro/lib/numberpicker.dart
+++ gains_tracker_pro/lib/numberpicker.dart
@@ -63,7 +63,7 @@ class _NumberPickerScreenState extends State<NumberPickerScreen> {
 
   int toSeconds(double display) {
     int seconds = ((display.floor() * 60) + 
-                  ((display - display.floor()) *100)).toInt();
+                  ((display - display.floor()) * 100)).toInt();
     return seconds;
   }
    
@@ -78,143 +78,139 @@ class _NumberPickerScreenState extends State<NumberPickerScreen> {
             color: const Color.fromARGB(81, 136, 162, 165),
             borderRadius: const BorderRadius.all(Radius.circular(20)),
             child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget> [
-              TextButton(
-                onPressed: () {
-                  if (!buttonNull) {
-                    if (timerRun == false && _seconds > 0) {
-                      startTimer();
-                      buttonNull = true;
-                    } else {
-                      cancelTimer();
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget> [
+                TextButton(
+                  onPressed: () {
+                    if (!buttonNull) {
+                      if (timerRun == false && _seconds > 0) {
+                        startTimer();
+                        buttonNull = true;
+                      } else {
+                        cancelTimer();
+                      }
                     }
-                  }
-                }, 
-                style: ButtonStyle(backgroundColor: Theme.of(context).brightness == Brightness.dark
-                          ? const MaterialStatePropertyAll(Color.fromARGB(255, 47, 47, 47))
-                          : const MaterialStatePropertyAll(Color.fromARGB(255, 245, 245, 245)),
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30)
-                            )
-                            
-                            
-                          )),
-                child: Text(_curEmoji),
-              ),
-              DecimalNumberPicker(
-                minValue: 0,
-                haptics: true,
-                maxValue: 30,
-                itemWidth: 40,
-                value: toDisplay(_seconds),
-                decimalPlaces: 2,
-                textStyle: const TextStyle(color: Colors.grey),
-                selectedTextStyle: TextStyle(
-                  color: Theme.of(context).brightness == Brightness.dark
-                          ? Colors.white
-                          : Colors.black,
+                  }, 
+                  style: ButtonStyle(backgroundColor: Theme.of(context).brightness == Brightness.dark
+                    ? const MaterialStatePropertyAll(Color.fromARGB(255, 47, 47, 47))
+                    : const MaterialStatePropertyAll(Color.fromARGB(255, 245, 245, 245)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)
+                      )
+                    )
+                  ),
+                  child: Text(_curEmoji),
                 ),
-                onChanged: (value) => setState(() {
-                  if (timerRun) {
-                    _restTime = toDisplay(_seconds);
-                  } else {
-                    if (value > 20) {
-                      value = 20;
-                      _restTime = 20;
-                      _seconds = 1200;
-                      HapticFeedback.selectionClick();
+                DecimalNumberPicker(
+                  minValue: 0,
+                  haptics: true,
+                  maxValue: 30,
+                  itemWidth: 40,
+                  value: toDisplay(_seconds),
+                  decimalPlaces: 2,
+                  textStyle: const TextStyle(color: Colors.grey),
+                  selectedTextStyle: TextStyle(
+                    color: Theme.of(context).brightness == Brightness.dark
+                      ? Colors.white
+                      : Colors.black,
+                  ),
+                  onChanged: (value) => setState(() {
+                    if (timerRun) {
+                      _restTime = toDisplay(_seconds);
                     } else {
-                      setState(() {
-                      _restTime = value;
-                      _seconds = toSeconds(_restTime);
-                      });
+                      if (value > 20) {
+                        value = 20;
+                        _restTime = 20;
+                        _seconds = 1200;
+                        HapticFeedback.selectionClick();
+                      } else {
+                        setState(() {
+                          _restTime = value;
+                          _seconds = toSeconds(_restTime);
+                        });
+                      }
                     }
-                  }
-                })
-              )                
-            ]
-          ),
+                  })
+                )                
+              ]
+            ),
           ),
           const Spacer(flex: 1),
-
           Material(
             color: const Color.fromARGB(81, 136, 162, 165),
             borderRadius: const BorderRadius.all(Radius.circular(20)),
-          child: Row(
-            children: <Widget> [
-              Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget> [
-              Container(
-                padding: const EdgeInsets.only(bottom: 13),
-                child: const Icon(Icons.fitness_center)
-              ),
-              NumberPicker(
-                minValue: 0,
-                maxValue: 1000,
-                haptics: true,
-                itemWidth: 45,
-                value: _curWeight,
-                textStyle: const TextStyle(color: Colors.grey),
-                selectedTextStyle: TextStyle(
-                  color: Theme.of(context).brightness == Brightness.dark
+            child: Row(
+              children: <Widget> [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget> [
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 13),
+                      child: const Icon(Icons.fitness_center)
+                    ),
+                    NumberPicker(
+                      minValue: 0,
+                      maxValue: 1000,
+                      haptics: true,
+                      itemWidth: 45,
+                      value: _curWeight,
+                      textStyle: const TextStyle(color: Colors.grey),
+                      selectedTextStyle: TextStyle(
+                        color: Theme.of(context).brightness == Brightness.dark
                           ? Colors.white
                           : Colors.black,
+                      ),
+                      onChanged: (value) => setState(() { _curWeight = value; HapticFeedback.selectionClick();})
+                    ),
+                  ],
                 ),
-                onChanged: (value) => setState(() { _curWeight = value; HapticFeedback.selectionClick();})
-              ),
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget> [
-              Container(
-                padding: const EdgeInsets.only(bottom: 13),
-                child: const Icon(Icons.tag)
-              ),
-              NumberPicker(
-                minValue: 0,
-                maxValue: 125,
-                value: _curReps,
-                haptics: true,
-                itemWidth: 45,
-                textStyle: const TextStyle(color: Colors.grey),
-                selectedTextStyle: TextStyle(
-                  color: Theme.of(context).brightness == Brightness.dark
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget> [
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 13),
+                      child: const Icon(Icons.tag)
+                    ),
+                    NumberPicker(
+                      minValue: 0,
+                      maxValue: 125,
+                      value: _curReps,
+                      haptics: true,
+                      itemWidth: 45,
+                      textStyle: const TextStyle(color: Colors.grey),
+                      selectedTextStyle: TextStyle(
+                        color: Theme.of(context).brightness == Brightness.dark
                           ? Colors.white
                           : Colors.black,
+                      ),
+                      onChanged: (value) => setState(() => {_curReps = value, HapticFeedback.selectionClick()})
+                    ),
+                  ]
                 ),
-                onChanged: (value) => setState(() => {_curReps = value, HapticFeedback.selectionClick()})
-              ),
-            ]
-          ),
-            ]),
-          ),
-              Container(padding: const EdgeInsets.only(left: 30), 
-              child: MaterialButton(
-                color: Theme.of(context).brightness == Brightness.dark
-                          ? Colors.white
-                          : Colors.black,
-                shape: const CircleBorder(),
-                textColor: Theme.of(context).brightness == Brightness.dark
-                          ? Colors.black
-                          : Colors.white,
-                child: Icon(Icons.check, color: Theme.of(context).brightness == Brightness.dark
-                          ? Colors.black
-                          : Colors.white),
-                
-                onPressed: () {
-                  Set newSet = Set(_curReps, _curWeight);
-                  widget.callback(newSet);
-                },
-
-              ),
+              ]
             ),
-        ],
           ),
-        
-      );
+          Container(padding: const EdgeInsets.only(left: 30), 
+            child: MaterialButton(
+              color: Theme.of(context).brightness == Brightness.dark
+                ? Colors.white
+                : Colors.black,
+              shape: const CircleBorder(),
+              textColor: Theme.of(context).brightness == Brightness.dark
+                ? Colors.black
+                : Colors.white,
+              child: Icon(Icons.check, color: Theme.of(context).brightness == Brightness.dark
+                ? Colors.black
+                : Colors.white),
+              onPressed: () {
+                Set newSet = Set(_curReps, _curWeight);
+                widget.callback(newSet);
+              },
+            ),
+          ),
+        ],
+      ),    
+    );
   }
 }
